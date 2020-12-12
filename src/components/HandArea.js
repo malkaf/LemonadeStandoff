e36042b9092f2f8774ab5105544dcaa16ba7756a
Malka Fink <finkmalky@gmail.com>
2020-12-13 00:37:40 +0200

    disable card after play action

--- src/components/HandArea.js
+++ src/components/HandArea.js
@@ -4,12 +4,23 @@ import Card from "./common/Card";
 import "../styles/styles.scss";
 
 const HandArea = (props) => {
-  const { handCards, cardOrientation = CardOrientation.DOWN, onClick } = props;
+  const {
+    handCards,
+    cardOrientation = CardOrientation.DOWN,
+    onClick,
+    disable,
+  } = props;
 
   return (
     <div className="hand-area-wrapper">
       {handCards.map((value, i) => (
-        <Card cardOrientation={cardOrientation} onClick={onClick} value={value} key={i}/>
+        <Card
+          cardOrientation={cardOrientation}
+          onClick={onClick}
+          value={value}
+          key={i}
+          disable={disable}
+        />
       ))}
     </div>
   );
